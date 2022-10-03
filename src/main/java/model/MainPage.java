eef29e6c9a4c0cd80c2e12d1c13e91f0a01524ee
svrv-pvl <svrv.pvl@gmail.com>
2022-10-04 00:51:07 +0300

    added list of good cards

--- src/main/java/model/MainPage.java
+++ src/main/java/model/MainPage.java
@@ -1,22 +1,41 @@
 package model;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import model.pageParts.GoodShortCard;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage {
-    public GoodShortCard leaderCard;
+    public List<GoodShortCard> leaderCards;
 
     public MainPage(){
-        By nameLinkXPath = By.xpath("//ul[1]/li[1]/div/div[1]/a[1]");
-        By imageXPath = By.xpath("//ul/li[1]/div/div[2]/div[1]/a/img");
-        By addToBasketButtonXPath = By.xpath("//*[@id=\"hit_29354\"]");
 
-        leaderCard = new GoodShortCard(
+
+        /*leaderCard = new GoodShortCard(
                 $(nameLinkXPath),
                 $(imageXPath),
-                $(addToBasketButtonXPath)
-        );
+                $(addToBasketButtonXPath)*/
+        leaderCards = new ArrayList<>();
+
+        By nameLinkXPath = By.xpath(".//div/div[1]/a[1]");
+        By imageXPath = By.xpath(".//div/div[2]/div[1]/a/img");
+        By addToBasketButtonXPath = By.linkText("купить");
+        ElementsCollection leaderGoods = $$(By.xpath("//article/div[1]/div[3]/ul/li"));
+
+        System.out.println(leaderGoods.toString());
+
+        for(int i = 0; i < leaderGoods.size(); i++){
+            SelenideElement nameLink = leaderGoods.get(i).find(nameLinkXPath);
+            SelenideElement image = leaderGoods.get(i).find(imageXPath);
+            SelenideElement addToBasketButton = leaderGoods.get(i).find(addToBasketButtonXPath);
+
+            leaderCards.add(new GoodShortCard(nameLink,image, addToBasketButton));
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.codeborne.selenide.ElementsCollection;
 import model.MainPage;
+import model.pageParts.GoodShortCard;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -9,11 +10,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Program start");
 
-
-
         MainPage mainPage = open("http://drtechno.ru/", MainPage.class);
-        System.out.println(mainPage.leaderCard.getName());
-
+        System.out.println("--------------------Try to show from page object-------------");
+        for (GoodShortCard good:mainPage.leaderCards){
+            System.out.println(good.getName());
+        }
         System.out.println("Program finish");
     }
 }
